3f7882475af96823f79ea8a9ebfd61fc3acba1e0
Juan Rubio <jcrubio@gmail.com>
2025-09-18 10:33:03 -0500

    test: add a very simple test to start
    Signed-off-by: Juan Rubio <jcrubio@gmail.com>

---
+++ src/popup-utils.test.js
@@ -0,0 +1,22 @@
+// popup-utils.test.js
+const { timeSinceAccessText } = require('./popup-utils');
+
+describe('timeSinceAccessText', () => {
+  const now = 1_000_000_000_000; // fixed timestamp for deterministic tests
+
+  it('returns seconds ago for < 1 min', () => {
+    expect(timeSinceAccessText(now - 10_000, now)).toBe('10 seconds ago');
+  });
+
+  it('returns minutes ago for < 1 hour', () => {
+    expect(timeSinceAccessText(now - 120_000, now)).toBe('2 minutes ago');
+  });
+
+  it('returns hours ago for < 1 day', () => {
+    expect(timeSinceAccessText(now - 7_200_000, now)).toBe('2 hours ago');
+  });
+
+  it('returns days ago for >= 1 day', () => {
+    expect(timeSinceAccessText(now - 172_800_000, now)).toBe('2 days ago');
+  });
+});
